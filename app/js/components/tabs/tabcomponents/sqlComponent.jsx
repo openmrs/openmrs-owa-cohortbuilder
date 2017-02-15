62a0ac48777ff933d61715283d894b0d11fcdd03
Daniel Onodje <danielonodje@gmail.com>
2017-02-15 11:59:18 +0100

    add sql tab coponent
    add search history component
    
    add concept component tab
    
    Delete page component from the component root dir
    
    add sql tab component
    
    add rules to PR template

--- app/js/components/tabs/tabcomponents/sqlComponent.jsx
+++ app/js/components/tabs/tabcomponents/sqlComponent.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const SqlComponent = React.createClass({
     render: function(){
         return (
-            <h1>The Sql Component</h1>
+            <div className="content sql-component">
+                <textarea name="query" id="" cols="30" rows="10"></textarea>
+                <div className="button-div">
+                    <button className="btn">Search</button>
+                </div>
+                <h2 className="query-warning">
+                    <b>Important:</b> Please test your SQL queries in your favourite editor before using them here.
+                </h2>
+            </div>
         );
     }
 });

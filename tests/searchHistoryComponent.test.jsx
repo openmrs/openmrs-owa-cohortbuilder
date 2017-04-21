f348fedeec29c839e5b07a982e769842488f32f5
Azeez Olaniran <azeez.olaniran@andela.com>
2017-04-21 15:08:14 +0100

    Fix inconsistency of search results display for patient and encounter tab components

--- tests/searchHistoryComponent.test.jsx
+++ tests/searchHistoryComponent.test.jsx
@@ -8,12 +8,12 @@ describe('<SearchHistoryComponent />', () => {
 
     it('should render the BreadCrumbComponent in the dom', () => {
         sinon.spy(SearchHistoryComponent.prototype, 'render');
-        const wrapper = mount(<SearchHistoryComponent/>);
+        const wrapper = mount(<SearchHistoryComponent history={[]}/>);
         expect(SearchHistoryComponent.prototype.render.calledOnce).to.equal(true);
     });
 
     it('should contain the correct elements', () => {
-        const wrapper = shallow(<SearchHistoryComponent/>);
+        const wrapper = shallow(<SearchHistoryComponent history={[]}/>);
         expect(wrapper.find("div")).to.have.length(2);
         expect(wrapper.find("h3")).to.have.length(1);
     });

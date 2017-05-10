45d5ff5b5d54432cada5fe0e9e96dfdf4157641f
dkayiwa <dkayiwa@openmrs.org>
2017-05-10 09:54:50 -0500

    Revert "Implement search by location form in the encounter tab (#68)" (#69)
    This reverts commit 33c3877f6fc4fa015c0506cf1c95286dd86dc937.

--- app/js/components/tabs/tabcomponents/encounterComponent.jsx
+++ app/js/components/tabs/tabcomponents/encounterComponent.jsx
@@ -16,14 +16,10 @@ class EncounterComponent extends Component {
             currentPage: 1,
             toDisplay: [],
             totalPage: 0,
-            perPage: 10,
-            location: '',
-            method: 'ANY'
+            perPage: 10
         };
         this.searchByEncounter = this.searchByEncounter.bind(this);
         this.getFormValues = this.getFormValues.bind(this);
-        this.handleSelectOption = this.handleSelectOption.bind(this);
-        this.searchByLocation = this.searchByLocation.bind(this);
     }
 
     componentWillMount(){
@@ -150,84 +146,6 @@ class EncounterComponent extends Component {
         return label;
     }
 
-    /**
-     * Function to generate a location search description based on the search values
-     */
-    getLocationSearchDescription() {
-        // find the location name since we only have it's uuid
-        const selectedLocation = this.state.locations.find((item) => {
-             return item.id === this.state.location;
-        });
-        let searchDescription = `Patients in ${selectedLocation.value}`;
-        switch (this.state.method) {
-            case 'FIRST':
-                searchDescription += ' (by method EARLIEST_ENCOUNTER).';
-                break;
-            case 'LAST': 
-                searchDescription += ' (by method LATEST_ENCOUNTER).'; 
-                break;
-            default :
-                searchDescription += ' (by method ANY_ENCOUNTER).';
-                break;
-        }
-        return searchDescription;
-    }
-
-    /**
-     * Method to handle search by location submit events and search for patients 
-     * from the back end using location and method selected
-     * @param {Object} event - Object containing data about the triggered event
-     * @return {undefined}
-     */
-    searchByLocation(event) {
-        event.preventDefault();
-        const searchParameters = {
-            encounterSearchAdvanced : [
-              { name: 'locationList', value: [this.state.location] },
-              { name: 'timeQualifier', value: this.state.method },
-            ]
-        };
-        this.props.search(searchParameters).then(results => {
-            const allEncounterTypes = results.rows || [];
-            this.props.addToHistory(this.getLocationSearchDescription(), allEncounterTypes, results.query);
-            // reset fields to default
-            this.setState({ location: '', method: 'ANY' });
-        });
-    }
-
-    /**
-     * Method to handle option selection in select fields. It sets the selected option
-     * value to the property in state (referred to by the option id)
-     * @param {Object} event - Object contatining data about the triggered event
-     * @return {undefined}
-     */
-    handleSelectOption(event) {
-        event.preventDefault();
-        this.setState({ [event.target.id]: event.target.value });
-    }
-
-    /**
-     * Method to get an array of <option> element items from locations in the state
-     * @return {Array} - An array containing option elements of availaible methods
-     */
-    getLocationOptions() {
-        return this.state.locations.map((item) => {
-            return <option value={item.id} key={shortId.generate()}>{item.value}</option>;
-        });
-    }
-
-    /**
-     * Method to get an array of <option> element items from methods in the state
-     * @return {Array} - An array containing option elements of availaible methods
-     */
-    getMethodOptions() {
-        return [
-            <option key={shortId.generate()} value={'ANY'}>Any Encounter</option>,
-            <option key={shortId.generate()} value={'LAST'}>Most Recent Encounter</option>,
-            <option key={shortId.generate()} value={'FIRST'}>Earliest Encounter</option>
-        ];
-    }
-
     render(){
         return (
             <div className="encounter-component">
@@ -302,33 +220,6 @@ class EncounterComponent extends Component {
                   </div>
                 </form>
                 <hr/>
-                <h3>Search By Location</h3>
-                <form className="form-horizontal" onSubmit={this.searchByLocation}>
-                    <div className="form-group">
-                        <label htmlFor="gender" className="col-sm-2 control-label">Patients belonging to?:</label>
-                        <div className="col-sm-6">
-                            <select className="form-control" id="location" onChange={this.handleSelectOption} value={this.state.location}>
-                                <option value="">--Select Location--</option>
-                                {this.getLocationOptions()}
-                            </select>
-                        </div>
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="gender" className="col-sm-2 control-label">According to Method:</label>
-                        <div className="col-sm-6">
-                            <select className="form-control" id="method" onChange={this.handleSelectOption} value={this.state.method}>
-                                {this.getMethodOptions()}
-                            </select>
-                        </div>
-                    </div>
-
-                    <div className="form-group">
-                        <div className="col-sm-offset-2 col-sm-6">
-                            <button type="submit" className="btn btn-success">Search</button>
-                        </div>
-                    </div>
-                </form>
               </div>
             </div>
         );

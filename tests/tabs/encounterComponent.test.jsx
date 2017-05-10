d03b4f326539d086267a4fa085ba6c4d16cb8b1e
Azeez Olaniran <azeez.olaniran@andela.com>
2017-05-10 12:34:03 +0100

    Implement search by location form in the encounter tab

---
+++ tests/tabs/encounterComponent.test.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import {expect} from 'chai';
+import { fakeRequestLibrary } from '../apiHelper/fakeRequestLibrary';
+import EncounterComponent from '../../app/js/components/tabs/tabcomponents/encounterComponent';
+
+describe('<EncounterComponent /> tab component', () => {
+  let encounterComponent = null;
+  beforeEach(() => {
+    encounterComponent = shallow(<EncounterComponent fetchData={fakeRequestLibrary} />);
+  });
+
+  it('should render 2 forms', () => {
+    expect(encounterComponent.find('form')).to.have.length(2);
+  });
+
+  it('Should render two <h3></h3> headers for the two forms', () => {
+    expect(encounterComponent.find('h3')).to.have.length(2);
+  });
+
+  it('Search by location form should contain a search button', () => {
+    const locationform = encounterComponent.childAt(1).childAt(4);
+    expect(locationform.children().find('button')).to.have.length(1);
+    expect(locationform.children().find('button').at(0).props().type)
+      .to.equal('submit');
+  });
+
+  it('Search by location form should have 2 select fields', () => {
+    expect(encounterComponent.childAt(1).childAt(4).find('select')).to.have.length(2);
+  });
+
+  it('all nodes with class control-label is a label', () => {
+    expect(encounterComponent.find('.control-label').every('label')).to.be.true;
+  });
+
+});
--- tests/tabs/programmeComponent.test.jsx
+++ tests/tabs/programmeComponent.test.jsx
@@ -9,32 +9,25 @@ describe('ProgrammeComponent', () => {
     renderedComponent = shallow(<ProgrammeComponent />);
   });
 
-  it('Should render 2 forms', () => {
-    expect(renderedComponent.find('form')).to.have.length(2);
+  it('Should render 1 forms', () => {
+    expect(renderedComponent.find('form')).to.have.length(1);
   });
 
-  it('Should render two h3 headers for the two forms', () => {
-    expect(renderedComponent.find('h3')).to.have.length(2);
+  it('Should render 1 h3 headers for the form', () => {
+    expect(renderedComponent.find('h3')).to.have.length(1);
   });
 
-  it('Search by location form should contain a search button', () => {
+  it(`Search by programme enrollment and status form should contatin a search
+    button`, () => {
     const locationform = renderedComponent.childAt(1);
     expect(locationform.children().find('button')).to.have.length(1);
     expect(locationform.children().find('button').at(0).props().type)
       .to.equal('submit');
   });
 
-  it(`Search by programme enrollment and status form should contatin a search
-    button`, () => {
-    const locationform = renderedComponent.childAt(3);
-    expect(locationform.children().find('button')).to.have.length(1);
-    expect(locationform.children().find('button').at(0).props().type)
-      .to.equal('submit');
-  });
-
-  it(`Search by programme enrollment and status form should have 2 select
+  it(`Search by programme enrollment and status form should have 3 select
     fields`, () => {
-    expect(renderedComponent.childAt(3).find('select')).to.have.length(2);
+    expect(renderedComponent.childAt(1).find('select')).to.have.length(3);
   });
 
   it('Search by location form should form should have 3 select fields', () => {
